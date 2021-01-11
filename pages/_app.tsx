a9ffb4c0b3063d0443421625a54e9a637b59dbeb
ffriedl89 <fabian.friedl@dynatrace.com>
2021-01-11 21:16:21 +0100

    remove print switch since this does not work with next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,9 @@ import { ApolloProvider } from '@apollo/client'
 import { useApollo } from '../lib/apollo';
 import Head from 'next/head';
 import './app.css'
-import { useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
-  const [media, setMedia] = useState('print');
 
   return (
     <ApolloProvider client={apolloClient}>
@@ -16,10 +14,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="preload"
           as="style"
           href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400&family=EB+Garamond:wght@400&display=swap" />
-
-        <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400&family=EB+Garamond:wght@400&display=swap"
-          media={media} onLoad={ () =>setMedia('all')} />
       </Head>
       <Component {...pageProps} />
     </ApolloProvider>
